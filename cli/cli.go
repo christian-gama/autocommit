b09f3cb183e749d6a097713f16955d6441e0b84c
Christian Gama <christiangama.dev@gmail.com>
2023-06-09 14:37:55 -0300

    chore(cli): add reset command to delete configuration file

--- cli/cli.go
+++ cli/cli.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"github.com/christian-gama/autocommit/store"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +16,16 @@ var (
 func init() {
 	cmd.PersistentFlags().
 		BoolVarP(&verbose, "verbose", "v", false, "create a verbose commit message")
+
+	cmd.AddCommand(reset)
+}
+
+var reset = &cobra.Command{
+	Use:   "reset",
+	Short: "Reset the configuration",
+	Run: func(cmd *cobra.Command, args []string) {
+		store.NewStore().DeleteConfigFile()
+	},
 }
 
 // Execute executes the root command.
