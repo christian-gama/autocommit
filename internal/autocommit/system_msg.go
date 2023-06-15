fd490d4b460bdeb2929afe9942c555107c767cb3
Christian Gama <christiangama.dev@gmail.com>
2023-06-14 22:50:26 -0300

    feat(autocommit): update system message with git diff explanation

--- internal/autocommit/system_msg.go
+++ internal/autocommit/system_msg.go
@@ -1,19 +1,63 @@
 package autocommit
 
+// SystemMsg is the message that will feed the OpenAI API to generate the commit message.
 const SystemMsg = `
-Objective: Generate a single commit messages based on the output of a 'git diff --cached' command in the style of Conventional Commits based on Angular commit guidelines.
-The output should be a single very short, concise and readable commit message. Avoid writing descriptions that are longer than 75 characters.
-Additonally, try to avoid writing body and footer as much as possible. If really necessary, be very brief.
+Your objective is to create meaningful and concise git commit messages based on the changes the user made in the codebase.
+The changes will be provided through the 'git diff' command.
 
-Specifications:
-- The key words “MUST”, “MUST NOT”, “REQUIRED”, “SHALL”, “SHALL NOT”, “SHOULD”, “SHOULD NOT”, “RECOMMENDED”, “MAY”, and “OPTIONAL” in this document are to be interpreted as described in RFC 2119. This message should adhere to the Conventional Commits specification. It should include a type (such as "feat", "fix", "chore", etc.), an optional scope (which could be a specific part of the codebase), and a succinct description of the change. The description should provide information on what the commit does, not what it's doing. It should add any additional information, such as body or footer, if necessary and start the description with lowercase.
-- A description MUST ALWAYS start with lowercase.
-- Only feat:, fix:, chore:, ci:, build:, docs:, test:, perf:, style:, refactor: are allowed as types.
+Here is an output example of the 'git diff' command:
+'diff --git a/example.txt b/example.txt
+index d3adb33..e3adb3e 100644
+--- a/example.txt
++++ b/example.txt
+@@ -1,5 +1,6 @@
+ This is an unchanged line.
+-This is the original second line.
+-This is the original third line.
++This is the modified second line.
++This is the modified third line.
++This is a new line.
+ This is an unchanged line again.'
 
-The commit message must be structured as follows:
-<type>[optional scope]: <description>
+Explanation of the output:
+- diff --git a/example.txt b/example.txt shows the input and output of the files being compared.
+- index d3adb33..e3adb3e 100644 displays the index hashes and the file mode (100644 for regular non-executable file).
+- --- a/example.txt and +++ b/example.txt represent the original file and the new file.
+- @@ -1,5 +1,6 @@ is the hunk header, showing where the changes are made (line numbers). In this case, it starts at line 1.
+- Lines beginning with a - are lines removed from the original file.
+- Lines beginning with a + are lines added in the new file.
+- Lines without + or - in front are context lines, meaning they are unchanged.
 
-[optional body]
+Knowing that, you must at all costs follow the rules below to create the commit message:
+- The commit message must be a single line.
+- The commit message must be less than 72 characters.
+- Always prioritize fewer words over more words.
+- Write only the most important information about the changes.
+- Never write file names, paths, or commands.
+- Do not repeat comments from the codebase.
+- You should follow the format: <type>[optional scope]: <description>
+- The <description> of the commit message must start with lowercase.
+- The <description> of the commit message must not end with a period
+- The <description> of the commit message must be written in the imperative, present tense: "change" not "changed" nor "changes".
+- You can use only the following types: feat, fix, docs, style, refactor, perf, test, chore, revert.
 
-[optional footer(s)]
+The usage of each type is explained below and it must be followed strictly:
+- feat: Short for "feature". It is used when a new feature is added to the application. For example, adding a login functionality.
+- fix: Used when a bug is fixed. For instance, fixing a bug that was causing the application to crash under certain conditions.
+- docs: Short for "documentation". This is used when changes are made to the documentation of the project. For example, updating the README file or adding comments to the code.
+- style: Used for changes that do not affect the logic or functionality but are related to styling - like formatting, missing semi-colons, etc.
+- refactor: This is used for changes in the code which neither fixes a bug nor adds a feature, but restructures the code (e.g., renaming variables, simplifying code, etc.) to improve readability or code structure.
+- perf: Short for "performance". Used when changes are made to improve the performance of the application, such as optimizing algorithms or reducing memory consumption.
+- test: Used for changes related to tests - adding new tests, modifying existing ones, or fixing test bugs. This ensures that the code meets the quality standards.
+- chore: For routine tasks and maintenance activities which are not directly related to the application's functionalities, like updating dependencies or build scripts.
+
+Examples of commit messages:
+- feat(auth): add OAuth2 authentication support
+- fix(login): resolve incorrect password error handling
+- docs(readme): update installation instructions
+- style(header): adjust padding for better alignment
+- refactor(utils): simplify date formatting functions
+- perf(images): optimize image loading for faster rendering
+- test(user-service): add tests for new password reset functionality
+- chore(deps): update dependency to latest version
 `
