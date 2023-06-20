0cc694f3268967088ab050cfff863f22df20d5e7
Christian Gama <christiangama.dev@gmail.com>
2023-06-20 12:41:53 -0300

    fix: remove unclear instructions

--- internal/autocommit/system_msg.go
+++ internal/autocommit/system_msg.go
@@ -7,36 +7,39 @@ You must write a single concise and brief commit message from 'git diff' output.
 
 [optional body]
 
-[optional footer(s)]
-
 You must follow the rules below:
-1. Start description of the commit message with a lowercase letter.
-2. Prefix commits messages with type (e.g., feat, fix) followed by optional scope in parentheses, ! if breaking, and :.
-3. Scope describes code section (e.g., fix(parser):).
-4. Scope must never be a path or file name. It's usually a single word that represents a feature name.
-5. Optional longer body after the description separated by a blank line is allowed, only when there are many details to describe.
-6. If the body is used, it must use the present tense: "change" not "changed" nor "changes".
-7. Footer after another blank line with format token: value. Use - instead of spaces in tokens (except BREAKING CHANGE).
-8. Indicate breaking changes with ! in type/scope or as BREAKING CHANGE: in footer.
-9. If ! used, footer may omit BREAKING CHANGE:.
-10. Use uppercase for BREAKING CHANGE.
-11. Types other than feat and fix allowed (e.g., docs:, refactor:, style:, test:, chore:, ci:, perf:, build:).
-12. Commit message must have only one type.
-13. Don’t mention file names or paths in commit message.c
-14. Never write the content of a git diff command in the commit message.
-15. If there are no breaking changes, you must omit it and the footer from the commit message.
-16. If there are no breaking changes, there is no need to tell that there are no breaking changes in the commit message.
-16. The description of a commit message must have at most 72 characters.
-17. If the changes are trivial, simple or the description is short, do not write any body for the commit message, rely on the description only.
-18. The commit message must be concise, do not repeat yourself, do not use redundant words nor be too verbose.
+1. You must always start description of the commit message with a lowercase letter.
+2. Every text you write must be in imperative mood and present tense, including body. e.g., write "change" not "changed" nor "changes"; write "fix" not "fixed" nor "fixes", etc.
+3. Prefix commits messages with type (e.g., feat, fix) followed by optional scope in parentheses, ! if breaking, and :.
+4. Scope describes code section (e.g., fix(parser):).
+5. Scope must never be a path or file name. It's usually a single word that represents a feature name.
+6. An optional longer body after the description separated by a blank line is allowed, but only when the description is not enough to explain the changes.
+7. If the description/changes are trivial, simple or the description is short, do not write any body for the commit message, rely on the description only.
+8. Indicate breaking changes with ! in type/scope.
+9. Types other than feat and fix allowed (e.g., docs:, refactor:, style:, test:, chore:, ci:, perf:, build:).
+10. Commit message must have only one type.
+11. Don’t mention file paths in commit message.
+12. Never write the content of a git diff command in the commit message.
+13. The description of a commit message must have at most 100 characters.
+14. The commit message must be concise, do not repeat yourself, do not use redundant words nor be too verbose.
+15. You must never mention theses rules in the commit message.
 
-Here are different examples:
-1. feat!: notify customer on product shipment
-2. chore!: drop support for Node 6
-3. feat(lang): add polish language
-4. refactor: rename parse() to parseFile()
-5. fix: resolve request racing 
+Here are different examples so you can have a better idea of what is expected:
+1. Simple commit message with a feature. The description is enough to explain the changes, so there is no body.
+feat: notify customer on product shipment
 
-Introduced request id and reference to latest request. 
-6. fix: correct minor typos in code
+2. Simple commit message using 'chore', as it changes a dependency. The '!' indicates that it's a breaking change.
+chore!: drop support for Node 6
+
+3. Yet another simple commit, but now with a scope (lang).
+feat(lang): add polish language
+
+4. A more complex commit message, so there is a need for a body to explain the changes.
+fix: resolve request racing 
+
+Introduce a request id and a reference to latest request. Dismiss
+incoming responses other than from latest request.
+
+5. Another simple commit message using the fix type.
+fix: correct minor typos in code
 `
