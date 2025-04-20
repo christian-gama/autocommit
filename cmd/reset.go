08607cc44b6393b97996077d4dd33ffc5da30827
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-04-20 18:03:47 +0100

    refactor(variable): rename variable and remove import

--- cmd/reset.go
+++ cmd/reset.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/christian-gama/autocommit/internal/groq"
-	"github.com/christian-gama/autocommit/internal/llm"
 	"github.com/christian-gama/autocommit/internal/openai"
 	"github.com/christian-gama/autocommit/internal/provider"
 	"github.com/spf13/cobra"
@@ -20,16 +19,15 @@ func runReset(cmd *cobra.Command, args []string) {
 	llmProvider := loadProvider()
 	proiderFactory := provider.NewProviderFactory(llmProvider)
 	configRepo := proiderFactory.MakeConfigRepo()
-	var rsg llm.ResetConfigCommand
 	switch llmProvider.GetName() {
 	case "openai":
 		fmt.Println("⌛ Resetting OpenAI configuration file...")
-		rsg = openai.NewOpenAIResetConfigCommand(configRepo)
+		resetConfigCommand = openai.NewOpenAIResetConfigCommand(configRepo)
 	case "groq":
 		fmt.Println("⌛ Resetting Groq configuration file...")
-		rsg = groq.NewGroqResetConfigCommand(configRepo)
+		resetConfigCommand = groq.NewGroqResetConfigCommand(configRepo)
 	}
-	if err := rsg.Execute(); err != nil {
+	if err := resetConfigCommand.Execute(); err != nil {
 		panic(err)
 	}
 
