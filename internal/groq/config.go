eb6c0b8dbbf7cc81deb6239eebc2bcf59b0eff0b
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-04-20 23:34:55 +0100

    feat: add new models
    Introduce llama versatile and gemma models to allowed models list. Update constants with new model names.

--- internal/groq/config.go
+++ internal/groq/config.go
@@ -58,11 +58,15 @@ func NewSystem(message, name string) *System {
 }
 
 const (
-	DEEPSEEK = "deepseek-r1-distill-llama-70b"
-	LLAMA    = "llama3-70b-8192"
+	DEEPSEEK        = "deepseek-r1-distill-llama-70b"
+	LLAMA           = "llama3-70b-8192"
+	LLAMA_VERSATILE = "llama-3.3-70b-versatile"
+	GEMMA           = "gemma2-9b-it"
 )
 
 var AllowedModels = []string{
 	DEEPSEEK,
 	LLAMA,
+	LLAMA_VERSATILE,
+	GEMMA,
 }
